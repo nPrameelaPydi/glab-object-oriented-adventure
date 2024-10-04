c0e4f8fc2bf2f5dcda7529a60735df93baaf3a51
Prameela Pydi <nprameela.pydi@gmail.com>
2024-10-04 13:52:04 -0700

    added random rolling of a number

--- script.js
+++ script.js
@@ -27,8 +27,20 @@ adventurer['companion'] = { name: 'Leo', type: 'cat' };
 //console.log(adventurer);
 
 adventurer.companion.companion = { name: 'Frank', type: 'Flea', belongings: ['hat', 'sunglasses'] };
+//console.log(adventurer);
 
+//adventurer = {
+//    roll(mod = 0) {
+//        const result = Math.floor(Math.random() * 20) + 1 + mod;
+//        console.log(`${this.name} rolled a ${result}.`);
+//    }
+//}
 
+adventurer.roll = function (mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+}
 
 console.log(adventurer);
+console.log(adventurer.roll(2));
 
