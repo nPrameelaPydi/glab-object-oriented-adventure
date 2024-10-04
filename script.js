fd476064e2f7040cc870ed9cc1482d8dd64501e1
Prameela Pydi <nprameela.pydi@gmail.com>
2024-10-04 13:40:10 -0700

    added nested objects

--- script.js
+++ script.js
@@ -23,13 +23,12 @@ for (let i = 0; i < adventurer.inventory.length; i++) {
     console.log(adventurer.inventory[i]);
 }
 
-//const adventurer = {
-//    name: "Robin",
-//    health: 10,
-//    inventory: ["sword", "potion", "artifact"],
-//    companion: {
-//        name: "Leo",
-//        type: "Cat"
-//    }
-//}
+adventurer['companion'] = { name: 'Leo', type: 'cat' };
+//console.log(adventurer);
+
+adventurer.companion.companion = { name: 'Frank', type: 'Flea', belongings: ['hat', 'sunglasses'] };
+
+
+
+console.log(adventurer);
 
